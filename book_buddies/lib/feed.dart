5411a4c2cf9f5df523d2ba11e1764c93232f8cb4
Simon Chervenak <sarras305@gmail.com>
2024-02-19 16:38:49 -0500

    update look of feed

--- book_buddies/lib/feed.dart
+++ book_buddies/lib/feed.dart
@@ -61,32 +61,85 @@ class Post extends StatelessWidget {
 
   Icon messageTypeIcon() {
     if (messageType == 'reading') {
-      return Icon(Icons.book);
+      return const Icon(Icons.book, color: Colors.orange);
     } else if (messageType == 'finished') {
-      return Icon(Icons.check);
+      return const Icon(Icons.check, color: Colors.green);
     } else if (messageType == 'added') {
-      return Icon(Icons.add);
+      return const Icon(Icons.add, color: Colors.blue);
     } else {
-      return Icon(Icons.error);
+      return const Icon(Icons.error, color: Colors.red);
     }
+  }
+
+  String timeDelta() {
+    final now = DateTime.now();
+    final difference = now.difference(time);
+
+    final seconds = difference.inSeconds;
+    if (seconds < 60) {
+      return '${seconds}s';
+    }
+
+    final minutes = difference.inMinutes;
+    if (minutes < 60) {
+      return '${minutes}m';
+    }
+
+    final hours = difference.inHours;
+    if (hours < 24) {
+      return '${hours}h';
+    }
+
+    final days = difference.inDays;
+    if (days < 7) {
+      return '${days}d';
+    }
+
+    final weeks = (days / 7).round();
+    if (weeks < 4) {
+      return '${weeks}w';
+    }
+
+    final months = (weeks / 4).round();
+    if (months < 12) {
+      return '${months}mo';
+    }
+
+    final years = (months / 12).round();
+    return '${years}y';
   }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      clipBehavior: Clip.antiAlias,
-      child: Column(
-        children: [
-          ListTile(
-            leading: messageTypeIcon(),
-            title: Text('$username $messageType $book'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy').format(time),
-              style: TextStyle(color: Colors.black.withOpacity(0.6)),
-            ),
-          ),
-        ],
-      ),
-    );
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(children: [
+              Row(children: [
+                RichText(
+                  text: TextSpan(
+                    style: DefaultTextStyle.of(context)
+                        .style
+                        .copyWith(fontSize: 12),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: username,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text: ' $messageType',
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  timeDelta(),
+                  style: TextStyle(color: Colors.grey),
+                )
+              ]),
+              const SizedBox(height: 8),
+              Text(book),
+            ])));
   }
 }
